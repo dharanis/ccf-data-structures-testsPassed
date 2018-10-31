48f9306324f568d0f97fb4722d622545736894ad
Laith <laithmatalkah@yahoo.com>
2018-10-30 17:06:03 -0700

    passed test
    Test Cases for Store revenues

--- store1/exercise.js
+++ store1/exercise.js
@@ -4,12 +4,14 @@ var store1 = require('./store1-data.js');
 
 // Return the value '4.63' from store1
 function accessesingData1() {
+return store1["2015-01-08"][0][1];
 
 }
 
 
 // Return how many 'Mint Wafers' were sold on January 6th
 function accessesingData2() {
+return store1["2015-01-06"][1][2];
 
 }
 
@@ -20,15 +22,38 @@ function accessesingData2() {
 // Create a loop to read which candies were sold by store1 on Jan 8. After simply outputting the data, try creating an array that contains the candy names.
 function loopingData1() {
 
+let myarray=[];
+for(let i=0;i<store1["2015-01-08"].length;i++)
+{
+    myarray[i] = store1["2015-01-08"][i][0]
+}
+return myarray;
+
 }
 
 // Create a loop to count the total number of candies sold on Jan 10 at store1. Where do you have to initialize the counter variable? Why?
 function loopingData2() {
+let sum = 0;
+for(let i=0;i<store1["2015-01-10"].length;i++)
+{
+    sum = sum +store1["2015-01-10"][i][2]
+}
+return sum;
 
 }
 
 // Use `Object.keys()` to get an array of the dates that candies were sold at store1.
 function loopingData3() {
+
+//let prop=[];
+
+//for(let i in store1 ){
+
+return Object.keys(store1);
+
+//}
+
+//return prop
 
 }
 
@@ -36,11 +61,25 @@ function loopingData3() {
 // Use a loop to calculate the total number of candies sold at store1.
 function loopingData4() {
 
+keys=Object.keys(store1);
+let sum=0;
+for(let i=0;i<keys.length;i++){
+
+  for(j=0;j<store1[keys[i]].length;j++){
+
+  sum=sum+store1[keys[i]][j][2]
+  }
+
+
+}
+
+return sum;
+
 }
 
 // In the previous exercise, where did you have to initialize the counter variable? Why?
 function loopingData5() {
-  return 'Answer'
+  return 'To iterate through the property name'
 }
 
 // *********** CHALLENGE *********** \\
@@ -48,6 +87,18 @@ function loopingData5() {
 // Calculate the total revenue of the candies sold by store1 on January 10th.
 // Be sure to return a number! Example `14.55`
 function challenge1() {
+
+keys=Object.keys(store1);
+
+let rev=0.0;
+
+for(let i=0;i<store1["2015-01-10"].length;i++){
+
+  rev=rev+ parseFloat(store1["2015-01-10"][i][1])*store1["2015-01-10"][i][2]
+
+}
+
+return rev.toFixed(2);
   
 }
 
